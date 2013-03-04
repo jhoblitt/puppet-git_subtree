34ad1a55ccf575b44c94bcc39c89acdc8e6a748c
Joshua Hoblitt <jhoblitt@cpan.org>
2013-03-04 10:16:56 -0700

    add basic tests

---
+++ spec/classes/git_subtree_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'git_subtree' do
+  let(:title) { 'redhat' }
+  let(:facts) { {:osfamily=> 'RedHat', :lsbmajordistrelease => 6} }
+
+  context 'with no params' do
+    it do
+      should include_class('git_subtree') 
+      should contain_file('/usr/libexec/git-core/git-subtree').with({
+        'ensure'  => 'present',
+        'replace' => 'false',
+        'mode'    => '0755',
+      }) 
+    end
+  end
+end
+
+describe 'git_subtree' do
+  let(:title) { 'debian' }
+  let(:facts) { {:osfamily=> 'Debian'} }
+
+  context 'with no params' do
+    it do
+      expect {
+        should include_class('git_subtree') 
+      }.to raise_error(Puppet::Error, /^Module git_subtree is not supported on/)
+    end
+  end
+end
